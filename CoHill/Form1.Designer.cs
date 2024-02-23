c047c3ceabef1d6a93548028c7516ecad7157db2
SosGutnov <soslan.gutnov157@gmail.com>
2024-02-23 15:52:53 +0300

    Добавьте файлы проекта.

---
+++ CoHill/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace CoHill
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewMain = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewMain
+            // 
+            this.dataGridViewMain.AllowUserToAddRows = false;
+            this.dataGridViewMain.AllowUserToDeleteRows = false;
+            this.dataGridViewMain.AllowUserToResizeColumns = false;
+            this.dataGridViewMain.AllowUserToResizeRows = false;
+            this.dataGridViewMain.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewMain.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridViewMain.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dataGridViewMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewMain.ColumnHeadersVisible = false;
+            this.dataGridViewMain.Location = new System.Drawing.Point(290, 98);
+            this.dataGridViewMain.MultiSelect = false;
+            this.dataGridViewMain.Name = "dataGridViewMain";
+            this.dataGridViewMain.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dataGridViewMain.RowHeadersVisible = false;
+            this.dataGridViewMain.RowTemplate.Height = 50;
+            this.dataGridViewMain.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridViewMain.Size = new System.Drawing.Size(212, 194);
+            this.dataGridViewMain.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(178, 351);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(212, 48);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ШИфрр";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(43, 125);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(146, 29);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(43, 263);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(146, 29);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(39, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ввод";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(39, 236);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Вывод";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 424);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridViewMain);
+            this.Name = "Form1";
+            this.Text = "Шифр Хилла";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewMain;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ CoHill/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoHill
+{
+    public partial class Form1 : Form
+    {
+        public Dictionary<char, int> Alfavit = new Dictionary<char, int>();
+        public int[,] A;
+        public string InpString;
+        public int n;
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridViewMain.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.None;
+            dataGridViewMain.Height = 180;
+            n = 3;
+            for (int i = 0; i < n; i++)
+            {
+                dataGridViewMain.Columns.Add("Column" + i.ToString(), "");
+                dataGridViewMain.Rows.Add();
+                dataGridViewMain.Rows[i].Height = dataGridViewMain.Height / n-1;
+            }
+            char[] alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            for (int i = 0; i < alpha.Length; i++)
+            {
+                Alfavit.Add(alpha[i], (i+1) % alpha.Length);
+            }
+            //MessageBox.Show(Alfavit.ContainsKey('A').ToString());
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetMassA();
+            InpString = textBox1.Text;
+            while (InpString.Length % n != 0)
+            {
+                InpString += "Z";
+            }
+            InpString = InpString.ToUpper();
+            CreateHill1(InpString);
+        }
+
+        private void CreateHill1(string s)
+        {
+            while(s.Length % n != 0)
+            {
+                s += "Z";
+            }
+            char[] beta = s.ToCharArray();
+            int t = s.Length / n;
+            int[][] mda = new int[t][];
+            for (int i = 0; i < t; i++)
+            {
+                mda[i] = new int[n];
+                for (int j = 0; j < n; j++)
+                {
+                    mda[i][j] = Alfavit[beta[i*n+j]];
+                }
+            }//составил вектора из буковок)
+
+            int[][] newmda = new int[t][];
+            for (int i = 0; i < t; i++)
+            {
+                newmda[i] = Multi(A, mda[i]);
+            }
+            string res = "";
+            for (int i = 0; i < t; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    res += Alfavit.FirstOrDefault(x => x.Value==newmda[i][j]).Key;
+                }
+            }
+            textBox2.Text = res;
+        }
+
+        private int[] Multi(int[,] a, int[] b)
+        {
+            int[] res = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    sum += (a[i, j] * b[j]) % 26;
+                }
+                res[i] = sum % 26;
+            }
+            return res;
+        }
+
+        public void GetMassA()
+        {
+            A = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    A[i, j] = int.Parse(dataGridViewMain.Rows[i].Cells[j].Value.ToString());
+                }
+            }
+        }
+    }
+}
