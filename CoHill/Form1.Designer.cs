c6c2df19afe93f0f9f7d94c3310f9cbdafb8cf67
SosGutnov <soslan.gutnov157@gmail.com>
2024-03-06 00:21:28 +0300

    доделал нахождение обратной матрицы

--- CoHill/Form1.Designer.cs
+++ CoHill/Form1.Designer.cs
@@ -35,6 +35,11 @@ namespace CoHill
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.labelSos2 = new System.Windows.Forms.Label();
+            this.labelSos1 = new System.Windows.Forms.Label();
+            this.textBoxSos2 = new System.Windows.Forms.TextBox();
+            this.textBoxSos1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMain)).BeginInit();
             this.SuspendLayout();
             // 
@@ -49,8 +54,7 @@ namespace CoHill
             this.dataGridViewMain.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
             this.dataGridViewMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridViewMain.ColumnHeadersVisible = false;
-            this.dataGridViewMain.Location = new System.Drawing.Point(387, 121);
-            this.dataGridViewMain.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridViewMain.Location = new System.Drawing.Point(290, 98);
             this.dataGridViewMain.MultiSelect = false;
             this.dataGridViewMain.Name = "dataGridViewMain";
             this.dataGridViewMain.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
@@ -58,16 +62,15 @@ namespace CoHill
             this.dataGridViewMain.RowHeadersWidth = 51;
             this.dataGridViewMain.RowTemplate.Height = 50;
             this.dataGridViewMain.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
-            this.dataGridViewMain.Size = new System.Drawing.Size(283, 239);
+            this.dataGridViewMain.Size = new System.Drawing.Size(212, 194);
             this.dataGridViewMain.TabIndex = 0;
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(237, 432);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(213, 344);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(283, 59);
+            this.button1.Size = new System.Drawing.Size(212, 48);
             this.button1.TabIndex = 1;
             this.button1.Text = "ШИфрр";
             this.button1.UseVisualStyleBackColor = true;
@@ -76,29 +79,26 @@ namespace CoHill
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox1.Location = new System.Drawing.Point(57, 154);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Location = new System.Drawing.Point(43, 125);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(193, 34);
+            this.textBox1.Size = new System.Drawing.Size(146, 29);
             this.textBox1.TabIndex = 2;
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox2.Location = new System.Drawing.Point(57, 324);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Location = new System.Drawing.Point(43, 263);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(193, 34);
+            this.textBox2.Size = new System.Drawing.Size(146, 29);
             this.textBox2.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(52, 121);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(39, 98);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 29);
+            this.label1.Size = new System.Drawing.Size(56, 24);
             this.label1.TabIndex = 4;
             this.label1.Text = "Ввод";
             // 
@@ -106,25 +106,75 @@ namespace CoHill
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(52, 290);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(39, 236);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(87, 29);
+            this.label2.Size = new System.Drawing.Size(69, 24);
             this.label2.TabIndex = 5;
             this.label2.Text = "Вывод";
             // 
+            // labelSos2
+            // 
+            this.labelSos2.AutoSize = true;
+            this.labelSos2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelSos2.Location = new System.Drawing.Point(620, 236);
+            this.labelSos2.Name = "labelSos2";
+            this.labelSos2.Size = new System.Drawing.Size(69, 24);
+            this.labelSos2.TabIndex = 9;
+            this.labelSos2.Text = "Вывод";
+            // 
+            // labelSos1
+            // 
+            this.labelSos1.AutoSize = true;
+            this.labelSos1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelSos1.Location = new System.Drawing.Point(620, 98);
+            this.labelSos1.Name = "labelSos1";
+            this.labelSos1.Size = new System.Drawing.Size(56, 24);
+            this.labelSos1.TabIndex = 8;
+            this.labelSos1.Text = "Ввод";
+            // 
+            // textBoxSos2
+            // 
+            this.textBoxSos2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxSos2.Location = new System.Drawing.Point(624, 263);
+            this.textBoxSos2.Name = "textBoxSos2";
+            this.textBoxSos2.Size = new System.Drawing.Size(146, 29);
+            this.textBoxSos2.TabIndex = 7;
+            // 
+            // textBoxSos1
+            // 
+            this.textBoxSos1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxSos1.Location = new System.Drawing.Point(624, 125);
+            this.textBoxSos1.Name = "textBoxSos1";
+            this.textBoxSos1.Size = new System.Drawing.Size(146, 29);
+            this.textBoxSos1.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(624, 344);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(212, 48);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "РАСшифрр";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(811, 522);
+            this.ClientSize = new System.Drawing.Size(900, 424);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.labelSos2);
+            this.Controls.Add(this.labelSos1);
+            this.Controls.Add(this.textBoxSos2);
+            this.Controls.Add(this.textBoxSos1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridViewMain);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Шифр Хилла";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -142,6 +192,11 @@ namespace CoHill
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelSos2;
+        private System.Windows.Forms.Label labelSos1;
+        private System.Windows.Forms.TextBox textBoxSos2;
+        private System.Windows.Forms.TextBox textBoxSos1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- CoHill/Form1.cs
+++ CoHill/Form1.cs
@@ -14,6 +14,7 @@ namespace CoHill
     {
         public Dictionary<char, int> Alfavit = new Dictionary<char, int>();
         public int[,] A;
+        public int[,] Z;
         public string InpString;
         public int n;
         public int N = 26;
@@ -48,10 +49,10 @@ namespace CoHill
                 InpString += "Z";
             }
             InpString = InpString.ToUpper();
-            CreateHill1(InpString);
+            CreateHill1(A, InpString, textBox2);
         }
 
-        private void CreateHill1(string s)
+        private void CreateHill1(int[,] A, string s, TextBox textbox)
         {
             while(s.Length % n != 0)
             {
@@ -82,7 +83,7 @@ namespace CoHill
                     res += Alfavit.FirstOrDefault(x => x.Value==newmda[i][j]).Key;
                 }
             }
-            textBox2.Text = res;
+            textbox.Text = res;
         }
 
         private int[] Multi(int[,] a, int[] b)
@@ -119,10 +120,12 @@ namespace CoHill
 
         public void GetInverseMatrix(int[,] matrix)
         {
-            int[,] newMatrix = new int[n, n];
+            int[,] newMatrix;
             int opr = GetOpr(matrix); //находим определитель
             int invOpr = GetInverseOprforModule(opr, N); // обратное определителю по модулю
             //MessageBox.Show(opr.ToString()+ ',' + invOpr.ToString());
+            newMatrix = Transpose(GetInverseMatrix2(matrix, invOpr));
+            Z = newMatrix;
         }
 
         public int GetOpr(int[,] matrix)
@@ -145,6 +148,59 @@ namespace CoHill
                 }
             }
             return 1;
+        }
+
+        public int[,] GetInverseMatrix2(int[,] a, int opr)
+        {
+            var res = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    res[i, j] = (GetMinor(a, i, j)*opr*((i+j) % 2==0 ? 1:-1) + 100*N) % N;
+                }
+            }
+            return res;
+        }
+
+        public int GetMinor(int[,] mas, int a, int b)
+        {
+            List<int> newMatrix2x2 = new List<int>((n-1)*(n-1));
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i != a && j !=b)
+                    {
+                        newMatrix2x2.Add(mas[i, j]);
+                    }
+                }
+            }
+            return (newMatrix2x2[0] * newMatrix2x2[3] - newMatrix2x2[1] * newMatrix2x2[2] + 100*N) % N;
+        }
+
+        public int[,] Transpose(int[,] a)
+        {
+            var b = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    b[i, j] = a[j, i];
+                }
+            }
+            return b;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            InpString = textBoxSos1.Text;
+            while (InpString.Length % n != 0)
+            {
+                InpString += "Z";
+            }
+            InpString = InpString.ToUpper();
+            CreateHill1(Z, InpString, textBoxSos2);
         }
     }
 }
